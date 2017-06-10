62b9fe50ff24ec2366872c24f2be632e52d226f7
Johannes Schriewer <hallo@dunkelstern.de>
2017-06-10 23:03:29 +0000

    Update plasma effect for efficiency, still slow but now has a fps counter

--- plasma.py
+++ plasma.py
@@ -2,14 +2,20 @@ import math
 import time
 
 from display import Color, DisplayDriver
+ms = lambda: int(round(time.time() * 1000))
 
 framebuffer = DisplayDriver()
 
+sinus = [math.sin(i * 3.1415 / 180.0) for i in range(360)]
+
+target_fps = 30
+delay = ms()
 try:
 	t1 = 0; t1dir = 1
 	t2 = 0; t2dir = 1
 	t3 = 0; t3dir = 1
 	while True:
+		start = ms()
 		t1 = (t1 + 0.3 * t1dir)
 		if t1 > 20:
 			t1dir = -1
@@ -24,19 +30,24 @@ try:
 		for x in range(framebuffer.width):
 			for y in range(framebuffer.height):
 				hue = 4.0 \
-					  + math.sin(x / (19.0 + t1)) \
-					  + math.sin(y / (9.0 + t2) + t3) \
-					  + math.sin((x + y) / (25.0 + t1 + t2)) \
-					  + math.sin(
-							math.sqrt(
+					  + sinus[int((x / (19.0 + t1)) / 3.1415 * 180) % 360] \
+					  + sinus[int((y / (9.0 + t2) + t3) / 3.1415 * 180) % 360] \
+					  + sinus[int(((x + y) / (25.0 + t1 + t2)) / 3.1415 * 180) % 360] \
+					  + sinus[
+							int((math.sqrt(
 								x ** 2.0 + y ** 2.0
-							) / 8.0
-						)
-				color = Color.from_hsv(hue * 360.0, 1.0, 0.25)
-				framebuffer.set_pixel(x, y, color)
+							) / 8.0) / 3.1415 * 180) % 360
+						]
+				framebuffer.set_pixel_hsv(x, y, hue * 360.0, 1.0, 0.25)
 
 		framebuffer.present()
-		time.sleep(5.0 / 1000.0)
+		end = ms()
+
+		if (end - start < 1000.0 / target_fps):
+			time.sleep((1000.0 / target_fps - (end - start)) / 1000.0)
+		if (ms() - delay) >= 1000:
+			delay = ms()
+			print('Current fps: {}'.format(1000 / (ms() - start)))
 except KeyboardInterrupt:
 	pass
 
